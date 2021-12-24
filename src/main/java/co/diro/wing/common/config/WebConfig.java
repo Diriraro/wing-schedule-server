f182819d34ebeb534323d76e41096bef21a5bdf9
Diriraro <94skyoun@naver.com>
2021-12-24 15:20:08 +0900

    cors 관련 설정 추가 필요

--- src/main/java/co/diro/wing/common/config/WebConfig.java
+++ src/main/java/co/diro/wing/common/config/WebConfig.java
@@ -2,6 +2,7 @@ package co.diro.wing.common.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,6 +19,10 @@ public class WebConfig implements WebMvcConfigurer {
 	
 	@Autowired
     private JwtInterceptor jwtInterceptor;
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+	}
  
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
